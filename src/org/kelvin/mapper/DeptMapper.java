eefc08b1c957d36aa34f66e057b373d7393f09a3
Klei <leiyun01@gmail.com>
2015-11-30 23:04:18 +0800

    spring+mybatis
    spring+mybatis

---
+++ src/org/kelvin/mapper/DeptMapper.java
@@ -0,0 +1,11 @@
+package org.kelvin.mapper;
+
+import java.util.List;
+
+import org.kelvin.entity.Dept;
+
+public interface DeptMapper {
+	
+	public List<Dept> findAll();
+
+}
---
+++ src/org/kelvin/test/TestDeptMapper.java
@@ -0,0 +1,25 @@
+package org.kelvin.test;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+import org.kelvin.entity.Dept;
+import org.kelvin.mapper.DeptMapper;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestDeptMapper {
+
+	@Test
+	public void testFindAll() throws IOException {
+		String conf = "applicationContext.xml";
+		ApplicationContext aContext = new ClassPathXmlApplicationContext(conf);
+		DeptMapper mapper = aContext.getBean("deptMapper", DeptMapper.class);
+		List<Dept> list = mapper.findAll();
+		for (Dept dept : list) {
+			System.out.println(dept.getDeptno() + " " + dept.getDname());
+		}
+		
+	}
+}
